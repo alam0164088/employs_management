cc68e913b18bf3601ffbdb3ff8fb1ac8b242a600
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-05-03 07:23:10 +0600

    update file

--- employer_management/wsgi.py
+++ employer_management/wsgi.py
@@ -1,9 +1,6 @@
-"""
-WSGI config for employer_management project.
-"""
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employer_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employ_management.settings')
 
 application = get_wsgi_application()
