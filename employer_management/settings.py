b5dd7805c88fe4b3a7a7a20c28b76f4d3ff9e2cb
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-05-04 05:56:59 +0600

    Removed embedded git repository

--- employer_management/settings.py
+++ employer_management/settings.py
@@ -15,7 +15,13 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+
+# settings.py
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'employs-management.onrender.com', '*.onrender.com']
+
+
+
 
 
 
