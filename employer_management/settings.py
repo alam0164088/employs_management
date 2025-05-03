cb2dc2d0a701195e51b70349a3a53160e5b9c6c6
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-05-04 05:30:35 +0600

    add

--- employer_management/settings.py
+++ employer_management/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
 
-ALLOWED_HOSTS = ['employs-management.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
